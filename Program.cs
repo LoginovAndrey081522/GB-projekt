cc86fee3ed844d90762da9e2bafc7805b230fb80
EVRodygin <rodygin@gmail.com>
2022-10-17 13:00:28 +0700

    Mission Completed Its Work

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿string[] FillArray()
+﻿string[] workArray = FillArray();
+string[] resultArray = GenerateNewArray(workArray);
+string firstArray = PrintArray(workArray);
+string secondArray = PrintArray(resultArray);
+Console.WriteLine(firstArray + " -> " + secondArray);
+
+string[] FillArray()
 {
     Console.WriteLine("Введите данные через пробел, по окончании ввода нажмите Enter: ");
     string? enterSymbols = Console.ReadLine();
